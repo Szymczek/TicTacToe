9992b4d299764cd282c4feac402e6b0630697e1d
Szymczek <szymon.szymczewski@protonmail.com>
2021-09-10 12:19:53 +0200

    improved game, added class Player, modified game loop

--- main.py
+++ main.py
@@ -1,105 +1,82 @@
 # Config
-cross = 'X'
-circle = 'O'
-turn = cross
 end = False
-global move
 theBoard = {'TL': ' ', 'TM': ' ', 'TR': ' ',
             'ML': ' ', 'MM': ' ', 'MR': ' ',
             'LL': ' ', 'LM': ' ', 'LR': ' ', }
 moves = ["TL", "TM", "TR", "ML", "MM", "MR", "LL", "LM", "LR"]
 
 
-def print_board(board):
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.sign = ''
+        self.move = ''
+
+    def get_point(self):
+        self.score += 1
+
+    def set_sign(self, sign):
+        self.sign = sign
+
+    def input_move(self, ):
+        self.move = input("TL, TM, TR..\n")
+        if self.move not in moves:
+            print("Something went wrong, try again")
+            self.input_move()
+
+
+def print_board(board, player):
     print(board['TL'] + '|' + board['TM'] + '|' + board['TR'])
     print('-+-+-')
     print(board['ML'] + '|' + board['MM'] + '|' + board['MR'])
     print('-+-+-')
     print(board['LL'] + '|' + board['LM'] + '|' + board['LR'])
-    print('Turn for ' + turn + '. Move on which space?')
+    print('Turn for ' + player + '. Move on which space?')
 
 
-def small_brain_checks_win(board, _turn):
+def small_brain_checks_win(board, player):
     """ Checks possible wins, easy way"""
     global end
     # Diagonal
-    if board['TL'] == _turn and board['MM'] == _turn and board['LR'] == _turn:
-        print(_turn + "wins")
+    if board['TL'] == player.sign and board['MM'] == player.sign and board['LR'] == player.sign:
+        print(player.name + " wins")
+        player.get_point()
         end = True
-    if board['TR'] == _turn and board['MM'] == _turn and board['LL'] == _turn:
-        print(_turn + "wins")
+    if board['TR'] == player.sign and board['MM'] == player.sign and board['LL'] == player.sign:
+        print(player.name + " wins")
         end = True
     # Rows
-    if board['TL'] == _turn and board['TM'] == _turn and board['TR'] == _turn:
-        print(_turn + "wins")
+    if board['TL'] == player.sign and board['TM'] == player.sign and board['TR'] == player.sign:
+        print(player.name + " wins")
         end = True
-    if board['ML'] == _turn and board['MM'] == _turn and board['MR'] == _turn:
-        print(_turn + "wins")
+    if board['ML'] == player.sign and board['MM'] == player.sign and board['MR'] == player.sign:
+        print(player.name + " wins")
         end = True
-    if board['LL'] == _turn and board['LM'] == _turn and board['LR'] == _turn:
-        print(_turn + "wins")
+    if board['LL'] == player.sign and board['LM'] == player.sign and board['LR'] == player.sign:
+        print(player.name + " wins")
         end = True
     # Columns
-    if board['TL'] == _turn and board['ML'] == _turn and board['LL'] == _turn:
-        print(_turn + "wins")
+    if board['TL'] == player.sign and board['ML'] == player.sign and board['LL'] == player.sign:
+        print(player.name + " wins")
         end = True
-    if board['TM'] == _turn and board['MM'] == _turn and board['LM'] == _turn:
-        print(_turn + "wins")
+    if board['TM'] == player.sign and board['MM'] == player.sign and board['LM'] == player.sign:
+        print(player.name + " wins")
         end = True
-    if board['TL'] == _turn and board['MR'] == _turn and board['LR'] == _turn:
-        print(_turn + "wins")
+    if board['TL'] == player.sign and board['MR'] == player.sign and board['LR'] == player.sign:
+        print(player.name + " wins")
         end = True
 
 
-def check_win(board, _turn):
-    # Doesnt check columns
-    global end
-    x_counter = 0
-    zero_counter = 0
-    col_number = 0
-    for position in board:
-        col_number += 1
-        # Diagonal
-        if board['TL'] == _turn and board['MM'] == _turn and board['LR'] == _turn:
-            print(_turn + "wins")
-            end = True
-            break
-        if board['TR'] == _turn and board['MM'] == _turn and board['LL'] == _turn:
-            print(_turn + "wins")
-            end = True
-            break
-        if board[position] == cross:
-            x_counter += 1
-        if board[position] == circle:
-            zero_counter += 1
-        if col_number == 3:
-            col_number = 0
-            if x_counter == 3:
-                print(cross + " wins")
-                end = True
-            elif zero_counter == 3:
-                print(circle + " wins")
-                end = True
-            else:
-                x_counter = 0
-                zero_counter = 0
-
-
-def input_move():
-    global move
-    move = input("TL, TM, TR..\n")
-    if move not in moves:
-        print("Something went wrong, try again")
-        input_move()
-    return move
-
-
-def conflicts_check(moved):
-    if cross in theBoard[moved] or circle in theBoard[moved]:
+def conflicts_check(player, player2, sign1, sign2):
+    if player.move not in moves:
+        print("")
+        player.input_move()
+        conflicts_check(player, player2,  player.sign, player2.sign)
+    if sign1 in theBoard[player.move] or sign2 in theBoard[player.move]:
         print("You cant do this.. ")
-        global move
-        move = input_move()
-        conflicts_check(move)
+        player.input_move()
+        conflicts_check(player, player2,  player.sign, player2.sign)
 
 
 def tie():
@@ -108,26 +85,32 @@ def tie():
     end = True
 
 
-def start_game():
-    global turn
+def start_game(player1, player2):
     global theBoard
-    global move
     global end
-    for i in range(9):
-        print_board(theBoard)
-        move = input_move()
-        conflicts_check(move)
-        theBoard[move] = turn
-        small_brain_checks_win(theBoard, turn)
-        if i == 8:
+    player1.set_sign('X')
+    player2.set_sign('O')
+    for round in range(9):
+        if round == 8:
             tie()
         if end:
             break
-        if turn == cross:
-            turn = circle
+        if round % 2 == 0:
+            print_board(theBoard, player1.name)
+            player1.input_move()
+            conflicts_check(player1, player2, player1.sign, player2.sign)
+            theBoard[player1.move] = player1.sign
+            small_brain_checks_win(theBoard, player1)
         else:
-            turn = cross
+            print_board(theBoard, player2.name)
+            player2.input_move()
+            conflicts_check(player2, player1, player1.sign, player2.sign)
+            theBoard[player2.move] = player2.sign
+            small_brain_checks_win(theBoard, player2)
+
 
 
 # Start
-start_game()
+p1 = Player("Szymi")
+p2 = Player("Random")
+start_game(p1, p2)
